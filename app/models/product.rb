be022471aa118971a607ee3515721c89f0916c47
Victor Hugo de Oliveira Miranda <victor-olliveira@live.com>
2015-05-19 04:11:33 -0300

    Readd paperclip code for product and store models

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,16 @@ class Product < ActiveRecord::Base
   belongs_to :store
   
   has_attached_file :picture,
-                    :storage => :dropbox,
-                    :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png",
-                    :dropbox_options => { :path => proc { |style| "#{Rails.env}/#{style}/#{id}_#{picture.original_filename}"},
-                                          :unique_filename => true }
+                    :default_url => "/images/:style/missing.png"
+
+  # has_attached_file :picture,
+  #                   :storage => :dropbox,
+  #                   :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
+  #                   :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  #                   :default_url => "/images/:style/missing.png",
+  #                   :dropbox_options => { :path => proc { |style| "#{Rails.env}/#{style}/#{id}_#{picture.original_filename}"},
+  #                                         :unique_filename => true }
+
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
